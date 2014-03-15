d9ea798d1496b512d9762bd5c336fa7a763e13cc
Aleksej Kamynin <zendzirou@gmail.com>
2014-03-16 00:08:06 +0400

    Usage example for get_point_height method in comments.

--- bin/api.php
+++ bin/api.php
@@ -1,5 +1,7 @@
 <?php
 
 json_controller('get_point_height', function(){
+  // REQUEST: /get_point_height?latitude=52.4822&longitude=-1.9941
+  // RESPOND: 205
   return earthtools\get_point_height($_GET['latitude'], $_GET['longitude']);
 });
