33c5854649c1ee5c79597a217bdc24d5444f486c
Aleksej Kamynin <zendzirou@gmail.com>
2014-03-16 02:04:54 +0400

    Validations for get_point_height method

--- bin/api.php
+++ bin/api.php
@@ -3,6 +3,13 @@
 json_controller('get_point_height', function(){
   // REQUEST: /get_point_height?latitude=52.4822&longitude=-1.9941
   // RESPOND: 205
+
+  if(!isset($_GET['latitude']) or
+     !isset($_GET['longitude']) or
+     !preg_match('/^\-?\d+(\.\d+)$/', $_GET['latitude']) or
+     !preg_match('/^\-?\d+(\.\d+)$/', $_GET['longitude']))
+     bad_request('Missed or bad arguments.');
+  
   $height = earthtools\get_point_height($_GET['latitude'], $_GET['longitude']);
 
   HeightRequest::create(array(
--- helpers/controllers.php
+++ helpers/controllers.php
@@ -5,3 +5,12 @@ def('json_controller', function($path, $fn){
     echo json_encode($fn());
   });
 });
+
+def('bad_request', function($msg=''){
+  header('HTTP/1.1 400 Bad Request', true, 400);
+  if(is_dev_host())
+    echo $msg;
+  else
+    echo 'Bad Request';
+  exit;
+});
