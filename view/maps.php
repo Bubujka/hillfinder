16c9ae38d76a334cf5e88b8e9cad108fe21c5b1b
Aleksej Kamynin <zendzirou@gmail.com>
2014-03-16 19:18:20 +0400

    Added lat, lng field

--- view/maps.php
+++ view/maps.php
@@ -2,17 +2,30 @@
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
+
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript"
       src="http://maps.googleapis.com/maps/api/js?key=<?=google_api_key()?>&sensor=false">
     </script>
+
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
+    <script src="/public/js/main.js"></script>
+
     <script type="text/javascript">
-      function initialize() {
+      $(function() {
+        
+        var latInput = $('input[name=lat]')
+        var lngInput = $('input[name=lng]')
+
+        var defaultLatLng = new google.maps.LatLng(52.2032406, 20.9976958);
+
+        
         var mapOptions = {
-          center: new google.maps.LatLng(52.2032406,20.9976958),
+          center: defaultLatLng,
           zoom: 18,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
 
@@ -21,17 +34,27 @@
           map: map,
           title: 'Click to zoom'
         });
+
+        latInput.val(defaultLatLng.lat())
+        lngInput.val(defaultLatLng.lng())
+
         google.maps.event.addListener(map, 'click', function(event) {
           placeMarker(event.latLng);
         });
 
         function placeMarker(location) {
+          latInput.val(location.lat())
+          lngInput.val(location.lng())
           marker.setPosition(location)
-        }
-      }
+}
+      });
     </script>
   </head>
-  <body onload="initialize()">
+  <body>
     <div id="map_canvas" style="width:100%; height:100%"></div>
+    <div id='finder-pane'>
+      Latitude: <input type='text' name='lat'><br>
+      Longitude: <input type='text' name='lng'>
+    </div>
   </body>
 </html>
