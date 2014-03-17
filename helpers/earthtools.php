f45a93a8b6f5f5cb088b99194f13298a24daaeea
Aleksej Kamynin <zendzirou@gmail.com>
2014-03-17 12:44:09 +0400

    Bugfix =_=

--- helpers/earthtools.php
+++ helpers/earthtools.php
@@ -8,7 +8,7 @@
 deffc('earthtools\get_point_height', function($latitude, $longitude){
   $r = get_request('http://www.earthtools.org/height/'.floatval($latitude).'/'.floatval($longitude));
   $answer = (float)simplexml_load_string($r)->xpath('//meters/text()')[0];
-  if($answer = -9999)
+  if($answer == -9999)
     return null;
   return $answer;
 });
